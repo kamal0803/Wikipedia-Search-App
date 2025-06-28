a2aefc89bb781d352904194c615a8136b17bb658
Kamal Rajan Israni <kamalrajanisrani@Kamals-MacBook-Air.local>
2025-06-28 11:35:15 +0530

    Animation added

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 const div = document.querySelector(".container");
+const chase = document.querySelector(".sk-chase");
 
 document.querySelector("form").addEventListener("submit", function (e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const query = formData.get("query");
 
-  axios
-    .get(
-      `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${query}`
-    )
-    .then((res) => {
-      div.innerHTML = "";
+  chase.classList.remove("hidden");
 
-      for (let i = 0; i < res.data.query.search.length; i++) {
-        const ul = document.createElement("ul");
+  setTimeout(() => {
+    axios
+      .get(
+        `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${query}`
+      )
+      .then((res) => {
+        div.innerHTML = "";
 
-        const li1 = document.createElement("li");
-        li1.classList.add("title");
+        for (let i = 0; i < res.data.query.search.length; i++) {
+          const ul = document.createElement("ul");
 
-        const a1 = document.createElement("a");
-        a1.href ="https://en.wikipedia.org/?curid=" + res.data.query.search[i].pageid;
-        a1.target = "_blank";
-        a1.textContent = res.data.query.search[i].title;
-        li1.append(a1);
+          const li1 = document.createElement("li");
+          li1.classList.add("title");
 
-        ul.append(li1);
+          const a1 = document.createElement("a");
+          a1.href = "https://en.wikipedia.org/?curid=" + res.data.query.search[i].pageid;
+          a1.target = "_blank";
+          a1.textContent = res.data.query.search[i].title;
+          li1.append(a1);
 
-        const li2 = document.createElement("li");
-        li2.classList.add("url");
+          ul.append(li1);
 
-        const a2 = document.createElement("a");
-        a2.href = "https://en.wikipedia.org/?curid=" + res.data.query.search[i].pageid;
-        a2.target = "_blank";
-        a2.textContent = "https://en.wikipedia.org/?curid=" + res.data.query.search[i].pageid;
-        li2.append(a2);
+          const li2 = document.createElement("li");
+          li2.classList.add("url");
 
-        ul.append(li2);
+          const a2 = document.createElement("a");
+          a2.href = "https://en.wikipedia.org/?curid=" + res.data.query.search[i].pageid;
+          a2.target = "_blank";
+          a2.textContent = "https://en.wikipedia.org/?curid=" + res.data.query.search[i].pageid;
+          li2.append(a2);
 
-        const li3 = document.createElement("li");
-        li3.classList.add("description");
-        li3.innerHTML = res.data.query.search[i].snippet + "...";
-        ul.append(li3);
+          ul.append(li2);
 
-        div.append(ul);
+          const li3 = document.createElement("li");
+          li3.classList.add("description");
+          li3.innerHTML = res.data.query.search[i].snippet + "...";
+          ul.append(li3);
 
-      }
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+          div.append(ul);
+        }
+
+        // Hide loader after rendering
+        chase.classList.add("hidden");
+      })
+      .catch((e) => {
+        console.log(e);
+        // Always hide loader on error too
+        chase.classList.add("hidden");
+      });
+  }, 1000); // delay in milliseconds
 });
